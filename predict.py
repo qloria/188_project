2e7b3c73e8819f3f06add1393257714171138ab9
qloria <gloriaxch@gmail.com>
2017-06-01 14:18:20 -0700

    updated predict
    more methods

--- predict.py
+++ predict.py
@@ -4,15 +4,18 @@ import numpy as np
 from sklearn import svm
 from sklearn import tree
 from sklearn.kernel_ridge import KernelRidge
+from sklearn import linear_model
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 np.set_printoptions(threshold='nan')
 
-#clf = svm.SVC()
+
 #clf = KernelRidge(alpha=1.0)
+#clf = linear_model.Ridge (alpha = .5)
+clf = svm.SVC(C=100)
 #clf = tree.DecisionTreeClassifier()
-clf = AdaBoostClassifier(n_estimators=100)
+#clf = AdaBoostClassifier(n_estimators=100)
 #clf = RandomForestClassifier(n_estimators=10)
 clf.fit(training_data.x_col, training_data.y_col)
 print("Finished fitting data.")
@@ -26,8 +29,10 @@ for i in range(0, len(prediction)):
     total = total + 1
     if prediction[i] == testing_data.y_col[i]:
         count = count + 1
-print("Ratio:"+str(float(count)/total))
 
+print("Prediction:")
 print(prediction)
+print("Expected outcome:")
 print(testing_data.y_col)
+print("Accuracy: "+str(float(count)/total))
 
